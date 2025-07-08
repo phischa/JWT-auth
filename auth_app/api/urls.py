fa54207d13ac97b8c179c7560c412e9d6ce6d095
phischa <ph.git@mailbox.org>
2025-07-08 11:21:09 +0200

    settings token lifetime

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import RegistrationView
+from .views import RegistrationView, HelloWorldView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
-urlpatterns = {
+urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
-}
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+
+    path('hello/',  HelloWorldView.as_view(), name='hello')
+]
--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from .serializers import RegistrationSerializer
 
@@ -21,3 +21,9 @@ class RegistrationView(APIView):
             return Response(data)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+class HelloWorldView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({'message': 'Hello World!'})
